cd1e3da883756a4cba43e98687b3bb517b8f78f4
Johannes Danielson <hannes16@live.se>
2025-05-06 14:23:16 +0200

    La till felaktiga tester

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -1,7 +1,7 @@
 const { Builder, By, until } = require('selenium-webdriver');
 require('geckodriver');
 
-const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
+const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../src/index.html';
 const defaultTimeout = 10000;
 let driver;
 jest.setTimeout(1000 * 60 * 5); // 5 minuter
@@ -22,6 +22,25 @@ test('The stack should be empty in the beginning', async () => {
     let stack = await driver.findElement(By.id('top_of_stack')).getText();
     expect(stack).toEqual("n/a");
 });
+
+test('Stack should be empty after pushing and then popping an element', async () => {
+    let push = await driver.findElement(By.id('push'));
+    await push.click();
+    let alert = await driver.switchTo().alert();
+    await alert.sendKeys("WOW");
+    await alert.accept();
+
+    let pop = await driver.findElement(By.id('pop'));
+    await pop.click();
+    alert = await driver.switchTo().alert();
+    await alert.accept();
+
+    let peek = await driver.findElement(By.id('peek'));
+    await peek.click();
+
+    let stack = await driver.findElement(By.id('top_of_stack')).getText();
+    expect(stack).toEqual('WOW');
+})
 
 describe('Clicking "Pusha till stacken"', () => {
     it('should open a prompt box', async () => {
--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,10 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('pop on stack with two or more elements returns the top element', () =>{
+    stack.push(1);
+    stack.push(2);
+    expect(stack.pop()).toBeUndefined();
+    expect(stack.pop()).toBe(1);
+})
